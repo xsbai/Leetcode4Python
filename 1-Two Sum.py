532fd22222bacebfda7ef33fdef6c6e6f09e8e1e
xsbai <xs.bai93@gmail.com>
2020-02-17 12:29:17 +0800

    Modified function description

--- 1-Two Sum.py
+++ 1-Two Sum.py
@@ -11,74 +11,75 @@
 	因为 nums[0] + nums[1] = 2 + 7 = 9
 	所以返回 [0, 1]
 """
-
-"""
-解题关键num2 = target - num1，是否也在 list 中，那么就需要运用以下两个方法：
-num2 in nums，返回 True 说明有戏
-nums.index(num2)，查找 num2 的索引
-"""
-def twoSum1(nums, target):
-    lens = len(nums)
-    j=-1
-    for i in range(lens):
-        if (target - nums[i]) in nums:
-            if (nums.count(target - nums[i]) == 1)&(target - nums[i] == nums[i]):#如果num2=num1,且nums中只出现了一次，说明找到是num1本身。
-                continue
-            else:
-                j = nums.index(target - nums[i],i+1) #index(x,i+1)是从num1后的序列后找num2                
-                break
-    if j>0:
-        return [i,j]
-    else:
-        return []
-
-"""
-在方法一基础上，num2 的查找并不需要每次从 nums 查找一遍，只需要从 num1 位置之前或之后查找即可。
-但为了方便 index 这里选择从 num1 位置之前查找：
-"""
-def twoSum2(nums, target):
-    lens = len(nums)
-    j=-1
-    for i in range(1,lens):
-        temp = nums[:i]
-        if (target - nums[i]) in temp:
-            j = temp.index(target - nums[i])
-            break
-    if j>=0:
-        return [j,i]
-
-
-"""
-通过字典来模拟哈希查询的过程。
-个人理解这种办法相较于方法一其实就是字典记录了 num1 和 num2 的值和位置，
-而省了再查找 num2 索引的步骤。
-"""
-def twoSum3(nums, target):
-    hashmap={}
-    for ind,num in enumerate(nums):
-        hashmap[num] = ind
-    for i,num in enumerate(nums):
-        j = hashmap.get(target - num)
-        if j is not None and i!=j:
+class Solution:
+    """
+    解题关键num2 = target - num1，是否也在 list 中，那么就需要运用以下两个方法：
+    num2 in nums，返回 True 说明有戏
+    nums.index(num2)，查找 num2 的索引
+    """
+    def twoSum1(self, nums: [int], target: int) -> [int]:
+        lens = len(nums)
+        j=-1
+        for i in range(lens):
+            if (target - nums[i]) in nums:
+                if (nums.count(target - nums[i]) == 1)&(target - nums[i] == nums[i]):#如果num2=num1,且nums中只出现了一次，说明找到是num1本身。
+                    continue
+                else:
+                    j = nums.index(target - nums[i],i+1) #index(x,i+1)是从num1后的序列后找num2                
+                    break
+        if j>0:
             return [i,j]
+        else:
+            return []
+
+    """
+    在方法一基础上，num2 的查找并不需要每次从 nums 查找一遍，只需要从 num1 位置之前或之后查找即可。
+    但为了方便 index 这里选择从 num1 位置之前查找：
+    """
+    def twoSum2(self, nums: [int], target: int) -> [int]:
+        lens = len(nums)
+        j=-1
+        for i in range(1,lens):
+            temp = nums[:i]
+            if (target - nums[i]) in temp:
+                j = temp.index(target - nums[i])
+                break
+        if j>=0:
+            return [j,i]
 
 
-"""
-类似方法二，不需要 mun2 不需要在整个 dict 中去查找。
-可以在 num1 之前的 dict 中查找，因此就只需要一次循环可解决。
-"""
-def twoSum4(nums, target):
-    hashmap={}
-    for i,num in enumerate(nums):
-        if hashmap.get(target - num) is not None:
-            return [i,hashmap.get(target - num)]
-        hashmap[num] = i #这句不能放在if语句之前，解决list中有重复值或target-num=num的情况
+    """
+    通过字典来模拟哈希查询的过程。
+    个人理解这种办法相较于方法一其实就是字典记录了 num1 和 num2 的值和位置，
+    而省了再查找 num2 索引的步骤。
+    """
+    def twoSum3(self, nums: [int], target: int) -> [int]:
+        hashmap={}
+        for ind,num in enumerate(nums):
+            hashmap[num] = ind
+        for i,num in enumerate(nums):
+            j = hashmap.get(target - num)
+            if j is not None and i!=j:
+                return [i,j]
+
+
+    """
+    类似方法二，不需要 mun2 不需要在整个 dict 中去查找。
+    可以在 num1 之前的 dict 中查找，因此就只需要一次循环可解决。
+    """
+    def twoSum4(self, nums: [int], target: int) -> [int]:
+        hashmap={}
+        for i,num in enumerate(nums):
+            if hashmap.get(target - num) is not None:
+                return [i,hashmap.get(target - num)]
+            hashmap[num] = i #这句不能放在if语句之前，解决list中有重复值或target-num=num的情况
 
 
 if __name__ == '__main__':
-	nums=[2, 7, 11, 15]
-	target=9
-	print(twoSum1(nums, target))
-	print(twoSum2(nums, target))
-	print(twoSum3(nums, target))
-	print(twoSum4(nums, target))
+    solution = Solution()
+    nums=[2, 7, 11, 15]
+    target=9
+    print(solution.twoSum1(nums, target))
+    print(solution.twoSum2(nums, target))
+    print(solution.twoSum3(nums, target))
+    print(solution.twoSum4(nums, target))
